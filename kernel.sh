14c9f617a2e938aff7b39652158456fcc67278eb
张飞 <tdzfeng@gmail.com>
2018-12-21 14:44:56 +0800

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -5,4 +5,6 @@ export SUBARCH="arm64"
 export CROSS_COMPILE=$PWD/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 make -C $PWD O=outdir ARCH=arm64 ugglite_msm8937_defconfig
 make -j4 -C $PWD O=outdir ARCH=arm64
-
+curl -O https://raw.githubusercontent.com/rockymadden/transfer-cli/master/src/transfer
+chmod +x transfer
+transfer upload -f outdir/arch/arm64/boot/

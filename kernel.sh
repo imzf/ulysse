9649e62888ed685f02c1484632f34ae9395eb39e
imzf <tdzfeng@gmail.com>
2018-12-30 12:44:55 +0800

    update

--- kernel.sh
+++ kernel.sh
@@ -3,7 +3,7 @@ mkdir -p outdir
 export ARCH="arm64"
 export SUBARCH="arm64"
 export CROSS_COMPILE=$PWD/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-make -C $PWD O=outdir ARCH=arm64 ugglite_miui_defconfig
+make -C $PWD O=outdir ARCH=arm64 ugg_miui_defconfig
 make -j4 -C $PWD O=outdir ARCH=arm64
 git clone https://github.com/imzf/lazyflasher.git
 cp -T outdir/arch/arm64/boot/Image.gz-dtb lazyflasher/Image.gz
